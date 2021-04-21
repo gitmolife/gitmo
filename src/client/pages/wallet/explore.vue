55a04027a2f9d6e2b8958365a85b283a2e500918
Jon <squid@sqdmc.net>
2021-04-20 20:51:17 -0400

    add not finsihed pages

---
+++ src/client/pages/wallet/explore.vue
@@ -0,0 +1,84 @@
+<template>
+    <div style="text-align: center; margin-top: 7px; margin-bottom: 30px;">
+
+			<MkContainer :body-togglable="true" class="_gap">
+				<template #header><Fa :icon="faTachometerAlt"/> Wallet Explorer - OHM</template>
+				<div class="_content">
+					<div class="_keyValue"><b>TxID</b><span class="monospace" style="font-size: 1.07em;">{{ tx.txid }}</span></div>
+				</div>
+				<div class="_content">
+					<div class="_keyValue"><b>Blockhash</b><span class="monospace" style="font-size: 1.07em;">{{ tx.blockhash }}</span></div>
+				</div>
+			</MkContainer>
+
+    </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from 'vue';
+import parseAcct from '@/misc/acct/parse';
+import Progress from '@client/scripts/loading';
+import { faOm } from '@fortawesome/free-solid-svg-icons';
+import { faBtc } from '@fortawesome/free-brands-svg-icons';
+import { query as urlQuery } from '../../../prelude/url';
+import { acct as getAcct } from '../../filters/user';
+import * as os from '@client/os';
+import QRCode from 'qrcode';
+
+export default defineComponent({
+  components: {
+
+  },
+
+  props: {
+    txid: {
+      type: String,
+      required: false
+    },
+  },
+
+  data() {
+    return {
+			tx: {
+				txid: "",
+				blockhash: "",
+			},
+		  url: "",
+			resp_message: "",
+			faBtc, faOm,
+    };
+  },
+
+  created() {
+    this.fetch();
+  },
+
+  methods: {
+    getAcct,
+
+    fetch() {
+			this.url = "http://explorer.ohm.sqdmc.net/tx/" + this.txid;
+    },
+
+  }
+
+});
+</script>
+
+<style lang="scss" scoped>
+
+.monospace {
+  font-family: Lucida Console, Courier, monospace;
+}
+.resp-message {
+  font-family: Lucida Console, Courier, monospace;
+	font-style: italic;
+	font-size: 15px;
+}
+.address-texts {
+	text-shadow: -1px 2px 7px rgba(21, 29, 33, 0.88);
+	font-style: underline;
+	opacity: 0.88;
+}
+
+</style>
---
+++ src/server/api/endpoints/wallet/explore.ts
@@ -0,0 +1,61 @@
+import $ from 'cafy';
+import define from '../../define';
+import { ApiError } from '../../error';
+import { Users } from '../../../../models';
+import { ID } from '@/misc/cafy-id';
+
+export const meta = {
+	tags: ['wallet'],
+
+	requireCredential: false as const,
+
+	params: {
+		userId: {
+			validator: $.optional.type(ID),
+			desc: {
+				'ja-JP': '対象のユーザーのID',
+				'en-US': 'Target user ID'
+			}
+		},
+
+		username: {
+			validator: $.optional.str
+		},
+
+		host: {
+			validator: $.optional.nullable.str
+		},
+	},
+
+	res: {
+		type: 'object' as const,
+		optional: false as const, nullable: false as const,
+		ref: 'Wallet',
+	},
+
+	errors: {
+		noSuchUser: {
+			message: 'No such user.',
+			code: 'NO_SUCH_USER',
+			id: '88b36214-5918-4cec-be59-df48a42c53d7'
+		}
+	},
+};
+
+export default define(meta, async (ps, me) => {
+	const user = await Users.findOne(ps.userId != null
+		? { id: ps.userId }
+		: { id: me.id });
+
+	if (user == null) {
+		throw new ApiError(meta.errors.noSuchUser);
+	}
+
+	let wallet: UserWalletAddress = (await UserWalletAddresses.findOne({ userId: user.id} ) as UserWalletAddress);
+
+	if (wallet) {
+		return wallet.address;
+	} else {
+		return '';
+	}
+});
