00f05c7fbd4ce4e967f8660536c2a4bea4961f8a
Squid <Squid@sqdmc.net>
2022-09-14 10:24:27 -0700

    migration patch

--- packages/backend/migration/1646549089450-update-notification-tips.js
+++ packages/backend/migration/1646549089450-update-notification-tips.js
@@ -1,10 +1,10 @@
 
-export class pollEndedNotification1646549089451 {
-    name = 'pollEndedNotification1646549089451'
+export class updateNotification1646549089450 {
+    name = 'updateNotification1646549089450'
 
     async up(queryRunner) {
         await queryRunner.query(`ALTER TYPE "public"."notification_type_enum" RENAME TO "notification_type_enum_old"`);
-        await queryRunner.query(`CREATE TYPE "public"."notification_type_enum" AS ENUM('follow', 'mention', 'reply', 'renote', 'quote', 'reaction', 'pollVote', 'pollEnded', 'receiveFollowRequest', 'followRequestAccepted', 'groupInvited', 'app', 'tipReceive', 'tipSent')`);
+        await queryRunner.query(`CREATE TYPE "public"."notification_type_enum" AS ENUM('follow', 'mention', 'reply', 'renote', 'quote', 'reaction', 'pollVote', 'receiveFollowRequest', 'followRequestAccepted', 'groupInvited', 'app', 'tipReceive', 'tipSent')`);
         await queryRunner.query(`ALTER TABLE "notification" ALTER COLUMN "type" TYPE "public"."notification_type_enum" USING "type"::"text"::"public"."notification_type_enum"`);
         await queryRunner.query(`DROP TYPE "public"."notification_type_enum_old"`);
     }
